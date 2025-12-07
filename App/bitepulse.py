b8b1668285f04a66abdbf1434595e587c8dbc244
Aktham Momani <aktham.momani81@gmail.com>
2025-12-07 01:09:48 -0800

    Update bitepulse.py

--- App/bitepulse.py
+++ App/bitepulse.py
@@ -455,7 +455,7 @@ with left_col:
         video_html_attrs={
             "style": {
                 "width": "100%",
-                "height": "100%",#f"{DISPLAY_HEIGHT_PX}px", #"100%",
+                "height": "auto",#f"{DISPLAY_HEIGHT_PX}px", #"100%",
                 "objectFit": "cover",#"cover",
                 "border": "0",
                 "borderRadius": "10px"
@@ -544,6 +544,7 @@ if summary:
 if getattr(getattr(webrtc_ctx, "state", None), "playing", False):
     time.sleep(0.2)
     _safe_rerun()
+
 
 
 
